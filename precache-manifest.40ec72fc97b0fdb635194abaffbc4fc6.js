b34c21b665ca023c42e0396a1baaacc07eac1c2d
Dung Huynh <dung.huynh@gourmetplus.com>
2020-09-04 09:54:41 +0700

    Updates

---
+++ precache-manifest.40ec72fc97b0fdb635194abaffbc4fc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5291d12ddabb1008de752b06aa29b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d5a9c38c14927da9e4",
+    "url": "/static/css/main.552615f4.chunk.css"
+  },
+  {
+    "revision": "c677c87e2f0b3fa4a218",
+    "url": "/static/js/2.d2d943c7.chunk.js"
+  },
+  {
+    "revision": "07d5a9c38c14927da9e4",
+    "url": "/static/js/main.7ce199b6.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/json-eidtor/precache-manifest.be46ff6aef8419786ecab6731c0d2755.js"
+  "/precache-manifest.40ec72fc97b0fdb635194abaffbc4fc6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/json-eidtor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
