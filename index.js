c88a780db4b2cfe7195ec80b68f8ec90d2c23986
Dung Huynh <dung.huynh@gourmetplus.com>
2020-09-04 10:14:35 +0700

    allow all request

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "*");
+	res.header('Access-Control-Allow-Credentials', true);
+	next();
+});
+
 app.post('/api/data', (req, res) => {
   res.send(req.body)
 })
