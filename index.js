f5458cf7b209a653f8c7a0209b8475840337d767
Dung Huynh <dung.huynh@gourmetplus.com>
2020-09-04 08:31:19 +0700

    specific port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.post('/api/data', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('listening on port 5000'));
+app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.get('/', (req, res) => {
   res.send('Hello word!!!');
